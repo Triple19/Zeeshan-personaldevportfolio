01f4f25f0b8a4c95faecb3fb87428a173e971196
Zeeshan Shaik <shanshaik742@gmail.com>
2026-07-15 13:20:08 -0500

    fixing pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://Triple19.github.io",
+  base: "/Zeeshan-personaldevportfolio/",
   vite: {
     plugins: [tailwindcss()],
   },
