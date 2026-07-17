b8db647179b362d7c4b9a51005770cb8288a7be1
Zeeshan Shaik <shanshaik742@gmail.com>
2026-07-17 11:45:14 -0500

    Changed some information

--- src/config.ts
+++ src/config.ts
@@ -10,27 +10,27 @@ export const siteConfig = {
     github: "https://github.com/Triple19",
   },
   aboutMe:
-    "My name is Zeeshan Shaik, and I am an <strong>incoming</strong> junior in high school. I am electrical engineering student interested in learning about computer programming, machine learning, and computer components. My hobbies consist of playing video games and reading books.",
+    "My name is Zeeshan Shaik, and I am an incoming junior in high school. I am electrical engineering student interested in learning about computer programming, machine learning, and computer components. My hobbies consist of playing video games and reading books.",
   skills: ["Java", "Javascript", "Angular", "Node.js", "Python"],
   projects: [
     {
       name: "Environmental Science Fair Project",
       description:
-        "A project dedicated to research and effects of Methane and other chemicals on the environment. Results of research were demonstrated using python programs. It was submitted for judge review at a state level competitions.",
+        "A project dedicated to research and effects of Methane and other chemicals on the environment, results of research were demonstrated using python programs, was submitted for judge review at a state level competition",
       link: "N/A",
       skills: ["Research", "Python"],
     },
     {
       name: "EduFlex",
       description:
-        "Demo website to practice coding skills using AI APIs.",
+        "Developed an application addressing lack of personalization in education using Angular, FastAPI, Python virtual environments, and Gemini APIs, while collaborating with 2 other students",
       link: "https://github.com/Triple19/Eduflex",
       skills: ["Typescript", "HTML", "Python"],
     },
     {
       name: "Waterpark Research Project",
       description:
-        "Independent Research Project conducted with two other peers to research the effects of business dynamics on the local community and city. Our group recieved judge review and we even coordinated with officials of the waterpark franchise as a whole. ",
+        "Independent Research Project conducted with two other peers to research the effects of business dynamics on the local community and city, recieved judge review and even coordinated with officials of the waterpark franchise as a whole. ",
       link: "N/A",
       skills: ["Research", "Teamwork", "Communication"],
     },
@@ -41,7 +41,7 @@ export const siteConfig = {
       title: "Intern",
       dateRange: "June 2026 - August 2026",
       bullets: [
-        "Learned important GitHub Skills", "Developed strong resume", "Strengthened team skills"
+        "Utilized Git for version control, participating in branching strategies, pull requests, and code reviews to maintain code quality allowing for efficient team integration and maximizing productivity", "Built personal website using Astro and Typescript featuring 3+ project showcases with responsive design, implementing Github auto deployment capabilities ", "Developed a Discord bot using application features collaborating effectively using Git and Github with a cooperative team to improve user experience and enhance app capabilities"
       ],
     },
  
@@ -52,9 +52,9 @@ export const siteConfig = {
       degree: "Sophomore",
       dateRange: "2024 - Present",
       achievements: [
-        "Taking many AP Classes",
-        "Present in Robotics and Engineering Clubs",
-        "Moving on to my 3rd year",
+        "Completed many AP Courses",
+        "Participant in Robotics and Engineering Clubs",
+        "Currently in 2nd year",
       ],
     },
 
